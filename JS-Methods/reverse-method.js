912f635271f9f2ed47124af3df3692fae12618a8
ahmad-ausaf0 <ahmadausaf0@gmail.com>
2024-05-14 10:28:12 +0530

    reverse-method

---
+++ JS-Methods/reverse-method.js
@@ -0,0 +1,9 @@
+//JS array method, works only on arrays with no parameters. Just reverses the order of an array.
+
+// var array = ['a', 'b', 'c', 'd'];
+// console.log(array.reverse());
+
+// var array = [1, 2 ,3 ,4];
+// console.log(array.reverse());
+var array = [];
+console.log(array.reverse());
