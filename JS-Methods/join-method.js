a38f4d88563f077874b7e0240d184243ed8905a3
ahmad-ausaf0 <ahmadausaf0@gmail.com>
2024-05-14 10:48:04 +0530

    join-method

---
+++ JS-Methods/join-method.js
@@ -0,0 +1,41 @@
+// The join() method joins the elements of an array into a string, and return a string.
+
+// It has only one parameter, which is used to specify what you want the elements to be seperated by. This is optional and default is the comma(,).
+
+var array = ['a', 'b', 'c', 'd'];
+// console.log(array.reverse());
+// console.log(array.join());
+// console.log(array.join('-'));
+// console.log(array.join(' '));
+var array1 = [1, 2 ,3 ,4];
+// console.log(array1.join());
+
+//Challenge #1 -> a] [b] [c] [d
+
+var arrayC = ['a', 'b', 'c', 'd'];
+// console.log(arrayC.join('] ['));
+
+//Challenge #2 -> excellent
+
+// var splitString = "tnellecxe";
+// var arrayChars = splitString.split('');
+// console.log(arrayChars);
+// var reverseArr = arrayChars.reverse();
+// console.log(reverseArr);
+// var joinStr = reverseArr.join('');
+// console.log(joinStr);
+
+// Challenge #3 -> Great work today
+
+var string = "today work Great";
+var splitStr = string.split(' ');
+console.log(splitStr);
+var reverse = splitStr.reverse();
+console.log(reverse);
+var joinIt = reverse.join(' ');
+console.log(joinIt);
+
+
+
+
+
